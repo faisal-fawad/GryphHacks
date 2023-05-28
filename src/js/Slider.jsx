e23e0e8f82f13c8f91366d95aab84d26480e95cb
zelchy <zzzelchy@gmail.com>
2023-05-28 15:36:00 -0400

    Complete website

--- src/js/Slider.jsx
+++ src/js/Slider.jsx
@@ -3,7 +3,7 @@ import styles from '../css/Slider.module.css'
 function Slider({change, name}) {
   return (
     <label className={styles.switch}>
-      <input type="checkbox" defaultChecked={true} onChange={() => change(name)}/>
+      <input type="checkbox" defaultChecked={false} onChange={() => change(name)}/>
       <span className={styles.slider}></span>
     </label>
   )
